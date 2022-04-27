dd8f350c7f72c9d6412450a1a9b0a567d03b4b9c
Cheyanne Kester <ckester@uoregon.edu>
2022-04-26 23:49:47 -0700

    deleted OLDS

--- Assets/Scripts/HealthBarManager.cs
+++ Assets/Scripts/HealthBarManager.cs
@@ -16,8 +16,7 @@ public class HealthBarManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+    
     }
 
     // Update is called once per frame
